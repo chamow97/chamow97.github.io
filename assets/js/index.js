9844c7f3247c7e58b6ebd0029b1c01d690d3748b
chamow97 <jchandramowli15039@it.ssn.edu.in>
2017-11-22 18:38:47 +0530

    Update What I do? :smile:

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,7 +74,38 @@ $(document).ready(function() {
 		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
 	});
 });
-
+var typed = new Typed('.typed', {
+    strings: ["C++", "Java", "C#", "Django", "Laravel", "C", "Bootstrap", "PHP"],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 0.001,
+    // time before typing starts
+    startDelay: 0,
+    // backspacing speed
+    backSpeed: 1,
+    // time before backspacing
+    backDelay: 1000,
+    // loop
+    loop: true,
+    // false = infinite
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+});
 
 function downAction(){
     $.fn.fullpage.moveSectionDown();
